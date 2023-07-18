3c8f8b4338f96a042d1a29386aab896f3a5d99ff
vanhalf <vh.win32@gmail.com>
2023-07-18 11:59:50 +0300

    create post and slug

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,7 +11,7 @@ export default function Index() {
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
       <div className="relative sm:pb-16 sm:pt-8">
           <div className='mx-auto mt-16 max-w-7xl text-center'>
-              <Link to="/post"
+              <Link to="/posts"
                 className="text-xl text-blue-600 underline">
                     Blog Posts
               </Link>
